cafa4d9718ef3d52b337bfd279a61f762a2385d8
Morgan Smith <MSMITH1270@augusta.edu>
2018-05-08 15:08:27 -0400

    chat

--- signr/Startup.cs
+++ signr/Startup.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.Owin;
-using Owin;
-
-[assembly: OwinStartupAttribute(typeof(signr.Startup))]
-namespace signr
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
         }
     }
 }
